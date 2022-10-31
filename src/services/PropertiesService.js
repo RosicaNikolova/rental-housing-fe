430bee5aff0c3dc1edf4f85b5a130be50ffb29e1
RosicaNikolova <rosicanikolova1402@gmail.com>
2022-10-31 20:05:33 +0100

    Property Info Section created

---
+++ src/services/PropertiesService.js
@@ -0,0 +1,32 @@
+import http from "../http-common";
+
+const getAll = () =>{
+    return http.get("/properties");
+}
+
+const get = id => {
+    return http.get(`/tutorials/${id}`)
+}
+
+const create = data => {
+    return http.post("/tutorials", data);
+  };
+
+const update = (id, data) => {
+    return http.put(`/tutorials/${id}`, data);
+  };
+
+  const remove = id => {
+    return http.delete(`/tutorials/${id}`);
+  };
+
+
+  const PropertiesService = {
+    getAll,
+    get,
+    create,
+    update,
+    remove
+  };
+
+  export default PropertiesService;
